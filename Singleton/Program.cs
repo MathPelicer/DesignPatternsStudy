56874efe10d4cc7b5f2a7378c2e518484c636365
Matheus L. G. Pelicer <matheuspelicer58@gmail.com>
2023-12-25 22:44:01 -0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://refactoring.guru/design-patterns/singleton/csharp/example#lang-features for more info
+
+Thread process1 = new Thread(() => TestSingleton("value 1"));
+Thread process2 = new Thread(() => TestSingleton("value 2"));
+
+process1.Start();
+process2.Start();
+
+process1.Join();
+process2.Join();
+
+static void TestSingleton(string value)
+{
+    Singleton.Singleton singleton = Singleton.Singleton.GetInstance(value);
+    Console.WriteLine(singleton.Value.ToString());
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private Singleton() { }
+
+        public static Singleton _instance;
+
+        private static readonly object _instanceLock = new object();
+
+        public static Singleton GetInstance(string value)
+        {
+            if(_instance == null)
+            {
+                // this lock makes the singleton thread safe
+                lock(_instanceLock)
+                {
+                    // the first thread to get after the lock gets to create the instance
+                    if(_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Value { get; set; }
+    }
+}
